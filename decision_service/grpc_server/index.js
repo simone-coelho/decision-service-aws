0b8a64c63790046af674c2a459439ac369304dd0
Mike Ng <mike.ng@optimizely.com>
2018-12-10 15:47:27 -0800

    feat: Add getVariation and isFeatureEnabled grpc endpoints.

--- decision_service/grpc_server/index.js
+++ decision_service/grpc_server/index.js
@@ -11,6 +11,8 @@ function startServer(address) {
   grpcServer = new grpc.Server()
   grpcServer.addService(packageObject.Optimizely.DecisionService.service, {
     activate,
+    getVariation,
+    getFeature
   })
   grpcServer.bind(address, grpc.ServerCredentials.createInsecure())
   grpcServer.start()
@@ -22,6 +24,22 @@ async function activate(call, callback) {
   let optlyInstance = await optimizely.getInstance()
   let variationKey = optlyInstance.activate(experimentKey, userId, userAttributes)
   let response = { variationKey }
+  callback(null, response)
+}
+
+async function getVariation(call, callback) {
+  let { experimentKey, userId, userAttributes = {} } = call.request
+  let optlyInstance = await optimizely.getInstance()
+  let variationKey = optlyInstance.getVariation(experimentKey, userId, userAttributes)
+  let response = { variationKey }
+  callback(null, response)
+}
+
+async function getFeature(call, callback) {
+  let { featureKey, userId, userAttributes = {} } = call.request
+  let optlyInstance = await optimizely.getInstance()
+  let isEnabled = optlyInstance.isFeatureEnabled(featureKey, userId, userAttributes)
+  let response = { isEnabled }
   callback(null, response)
 }
 
