531787a1c08b91ab13dc3e507e3fd9cff2c1a971
Pete Koomen <pete@optimizely.com>
2018-12-12 02:16:24 -0800

    Refactor loadtest.go for simplicity and fixes

--- loadtest_service/loadtest.go
+++ loadtest_service/loadtest.go
@@ -4,164 +4,119 @@ package main
 
 import (
   "fmt"
-  "io/ioutil"
+  vegeta "github.com/tsenart/vegeta/lib"
+  "os"
   "strconv"
   "time"
-  "os"
-  vegeta "github.com/tsenart/vegeta/lib"
 )
 
-const DECISION_SERVER_ENV = "DECISION_SERVER"
-const DEBUG_ENV = "DEBUG"
-const RATE_ENV = "RATE"
-const STARTUP_DELAY_ENV = "STARTUP_DELAY"
-const DURATION_ENV = "LOADTEST_DURATION_SEC"
-const TEST_NAME_ENV = "TEST_NAME"
-
-const DEFAULT_LOADTEST_DELAY = 0
-const DEFAULT_DECISION_SERVER = "localhost:9090"
-const DEFAULT_RATE = 10
-const DEFAULT_DURATION = 10
-const DEFAULT_TEST_NAME = "get_variation"
-
-type GlobalParams struct {
-  StartupDelay time.Duration      // Wait this long before starting load tests
-  TargetServer string             // The decision service protocol (e.g. http)
-  Debug bool
-}
-
 type LoadTestParams struct {
-  Requests []RequestParams        // A slice of RequestParam objects to use in the test
-  NumUsers int                    // The desired number of unique user_ids to simulate
-  RequestsPerSecond int           // The desired number of requests to send per second
-  Duration time.Duration          // The desired test duration
+  Debug        bool
+  Duration     time.Duration   // The desired test duration
+  NumUsers     int             // The desired number of unique user_ids to simulate
+  Rate         int             // The desired number of requests to send per second
+  Requests     []RequestParams // A slice of RequestParam objects to use in the test
+  StartupDelay time.Duration   // Wait this long before starting load tests
+  TargetServer string          // The decision service protocol (e.g. http)
+  TestName     string          // The name of the test
 }
 
 type RequestParams struct {
-  Method string                   // Request method (e.g. POST)
-  Path string                     // Request Path
-  Body string                     // Request Body (may be a template)
+  Method string // Request method (e.g. POST)
+  Path   string // Request Path
+  Body   string // Request Body (may be a template)
 }
 
-func main() {
-  // Initialize the global parameters
-  globalParams := getGlobalParams()
+/**************************************************************
+ *  Test Parameters
+ *
+ *  Set these environment variables to configure the test parameters:
+ *
+ *    DEBUG - If "True" write debug info to stdout
+ *    DURATION - duration of the test, in seconds
+ *    NUM_USERS - the number of unique user_ids to use in the test
+ *    RATE - target number of requests to send each second
+ *    STARTUP_DELAY - the number of seconds to wait before starting the test
+ *    TARGET_SERVER - the target of the load test, e.g. localhost:9090
+ *    TEST_NAME - Test name; this is not currently used
+ */
 
-  // Delay the test so that the decision service containers have time to spin up
-  fmt.Printf("Waiting %s for decision service to start.\n", globalParams.StartupDelay)
-  time.Sleep(globalParams.StartupDelay) // todo: be smarter about this
+// Maps environment variables to their default values
+var ENVIRONMENT_VARIABLE_DEFAULTS = map[string]string{
+  "DEBUG":         "true",
+  "DURATION":      "10",
+  "NUM_USERS":     "100",
+  "RATE":          "10",
+  "STARTUP_DELAY": "0",
+  "TARGET_SERVER": "localhost:9090",
+  "TEST_NAME":     "get_variation Test",
+}
 
-  // Initialize the load test parameters
-  rate := DEFAULT_RATE
-  overrideRateStr := os.Getenv(RATE_ENV)
-  if len(overrideRateStr) > 0 {
-    overrideRateInt, err := strconv.Atoi(overrideRateStr)
-    if err != nil {
-      fmt.Printf("Ignoring request to set rate to non-integer %s\n", overrideRateStr)
-    } else {
-      rate = overrideRateInt
-    }
+// Return the requested environment variable value, or the default if unset
+func getEnv(variable string) string {
+  val := os.Getenv(variable)
+  if len(val) == 0 {
+    val, _ = ENVIRONMENT_VARIABLE_DEFAULTS[variable]
   }
+  return val
+}
 
-  duration := DEFAULT_DURATION
-  durationEnvValue := os.Getenv(DURATION_ENV)
-  if len(durationEnvValue) > 0 {
-    durationInt, err := strconv.Atoi(durationEnvValue)
-    if err != nil {
-      fmt.Printf("Ignoring request to set rate to non-integer %s\n", durationEnvValue)
-    } else {
-      duration = durationInt
-    }
+// Return the requested environment variable value, or the default if unset
+func getEnvInt(variable string) int {
+  val := os.Getenv(variable)
+  if len(val) == 0 {
+    val, _ = ENVIRONMENT_VARIABLE_DEFAULTS[variable]
   }
-
-  testName := DEFAULT_TEST_NAME
-  testNameEnvValue := os.Getenv(TEST_NAME_ENV)
-  if len(testNameEnvValue) > 0 {
-    testName = testNameEnvValue
-  }
-  b, err := ioutil.ReadFile(fmt.Sprintf("/res/tests/%s.json", testName))
+  intVal, err := strconv.Atoi(val)
   if err != nil {
     panic(err)
   }
-  body := string(b)
+  return intVal
+}
 
-  testParams := LoadTestParams{
+// Initialize and return a LoadTestParams object
+func getTestParams() LoadTestParams {
+
+  return LoadTestParams{
+    Debug: (getEnv("DEBUG") == "True"),
     Requests: []RequestParams{
-                  RequestParams{
-                    Method: "POST",
-                    Path: "/rpc",
-                    Body: body,
-                  },
+      RequestParams{
+        Method: "POST",
+        Path:   "/rpc",
+        Body:   "{ \"get_variation\": { \"datafile_key\":\"DjJKKrG8NnRhSLRVvX8VS8\", \"experiment_key\":\"simple_test\", \"user_id\":\"%d\", \"attributes\": {\"test_user\":\"true\"} } }",
+      },
     },
-    NumUsers: 50,
-    RequestsPerSecond: rate,
-    Duration: time.Duration(duration) * time.Second,
+    NumUsers:     getEnvInt("NUM_USERS"),
+    Rate:         getEnvInt("RATE"),
+    Duration:     time.Duration(getEnvInt("DURATION")) * time.Second,
+    StartupDelay: time.Duration(getEnvInt("STARTUP_DELAY")) * time.Second,
+    TargetServer: getEnv("TARGET_SERVER"),
+    TestName:     getEnv("TEST_NAME"),
   }
-
-  // Print the test parameters
-  fmt.Printf("Beginning load test:\n")
-  displayTestParameters(testParams, globalParams)
-
-  // Run the load test
-  metrics := runLoadTest(testParams, globalParams)
-
-  // Print the results
-  fmt.Printf("Completed load test:\n")
-  displayTestResults(metrics)
 
 }
 
-// Initialize and return a GlobalParams object
-func getGlobalParams() GlobalParams {
-  globalParams := GlobalParams{
-    StartupDelay: DEFAULT_LOADTEST_DELAY * time.Second,
-    TargetServer: DEFAULT_DECISION_SERVER,
-    Debug: false,
-  }
-
-  startupDelayEnvValue := os.Getenv(STARTUP_DELAY_ENV)
-  if len(startupDelayEnvValue) > 0 {
-    overrideStartupDelay, err := strconv.Atoi(startupDelayEnvValue)
-    if err != nil {
-      fmt.Printf("Failed to parse STARTUP_DELAY_ENV to int: %s\n", startupDelayEnvValue)
-    } else {
-      globalParams.StartupDelay = time.Duration(overrideStartupDelay) * time.Second
-    }
-  }
-
-  targetServerValue := os.Getenv(DECISION_SERVER_ENV)
-  if len(targetServerValue) > 0 {
-    globalParams.TargetServer = targetServerValue
-  }
-
-  debugEnvValue := os.Getenv(DEBUG_ENV)
-  if debugEnvValue == "1" {
-    globalParams.Debug = true
-  }
-
-  return globalParams
-
-}
-
-
+/**************************************************************
+ *  Running the test with Vegeta
+ */
 
 // Use vegeta to run the load test as specified in the passed parameters
-func runLoadTest(testParams LoadTestParams, globalParams GlobalParams) vegeta.Metrics {
+func runLoadTest(testParams LoadTestParams) vegeta.Metrics {
 
   // Specify the rate of requests
-  rate := vegeta.Rate{Freq: testParams.RequestsPerSecond, Per: time.Second}
+  rate := vegeta.Rate{Freq: testParams.Rate, Per: time.Second}
 
   // Build a 'target' for each distinct user and method
   var targets []vegeta.Target
   for user_id := 0; user_id < testParams.NumUsers; user_id++ {
     for _, requestParams := range testParams.Requests {
       requestBody := fmt.Sprintf(requestParams.Body, user_id)
-      if globalParams.Debug {
+      if testParams.Debug {
         os.Stderr.WriteString(fmt.Sprintf("Request: %s\n", requestBody))
       }
       targets = append(targets, vegeta.Target{
         Method: requestParams.Method,
-        URL:    fmt.Sprintf("http://%s%s", globalParams.TargetServer, requestParams.Path),
+        URL:    fmt.Sprintf("http://%s%s", testParams.TargetServer, requestParams.Path),
         Body:   []byte(requestBody),
       })
     }
@@ -173,7 +128,7 @@ func runLoadTest(testParams LoadTestParams, globalParams GlobalParams) vegeta.Me
 
   var metrics vegeta.Metrics
   for res := range attacker.Attack(targeter, rate, testParams.Duration, "Decision service load test") {
-    if globalParams.Debug {
+    if testParams.Debug {
       os.Stderr.WriteString(fmt.Sprintf("Response: %s\n", res.Body))
     }
 
@@ -185,11 +140,15 @@ func runLoadTest(testParams LoadTestParams, globalParams GlobalParams) vegeta.Me
 
 }
 
+/**************************************************************
+ *  Displaying results
+ */
+
 // Display the test parameters
-func displayTestParameters(testParams LoadTestParams, globalParams GlobalParams) {
-  fmt.Printf("  Service: http://%s\n", globalParams.TargetServer)
+func displayTestParameters(testParams LoadTestParams) {
+  fmt.Printf("  Service: http://%s\n", testParams.TargetServer)
   fmt.Printf("  Number of users: %d\n", testParams.NumUsers)
-  fmt.Printf("  Target rate: %d/s\n", testParams.RequestsPerSecond)
+  fmt.Printf("  Target rate: %d/s\n", testParams.Rate)
   fmt.Printf("  Target duration: %s\n", testParams.Duration)
 }
 
@@ -199,17 +158,41 @@ func displayTestResults(metrics vegeta.Metrics) {
   fmt.Printf("  Duration: %s\n", metrics.Duration)
   fmt.Printf("  Rate: %f\n", metrics.Rate)
   fmt.Printf("  Latency [Mean, 50, 95, 99]: %s %s %s %s\n",
-            metrics.Latencies.Mean,
-            metrics.Latencies.P50,
-            metrics.Latencies.P95,
-            metrics.Latencies.P99)
+    metrics.Latencies.Mean,
+    metrics.Latencies.P50,
+    metrics.Latencies.P95,
+    metrics.Latencies.P99)
   fmt.Printf("  Success rate: %f\n", metrics.Success)
   fmt.Printf("  Status Codes:\n")
-  for code,count := range metrics.StatusCodes {
+  for code, count := range metrics.StatusCodes {
     fmt.Printf("    %s: %d\n", code, count)
   }
   fmt.Printf("  Errors:\n")
-  for _,err := range metrics.Errors {
+  for _, err := range metrics.Errors {
     fmt.Printf("    %s\n", err)
   }
+}
+
+/**************************************************************
+ *  Main
+ */
+
+func main() {
+  // Initialize the test parameters
+  testParams := getTestParams()
+
+  // Delay the test so that the decision service containers have time to spin up
+  fmt.Printf("Waiting %s for decision service to start.\n", testParams.StartupDelay)
+  time.Sleep(testParams.StartupDelay) // todo: be smarter about this
+
+  // Print the test parameters
+  fmt.Printf("Beginning load test (%s):\n", testParams.TestName)
+  displayTestParameters(testParams)
+
+  // Run the load test
+  metrics := runLoadTest(testParams)
+
+  // Print the results
+  fmt.Printf("Completed load test (%s):\n", testParams.TestName)
+  displayTestResults(metrics)
 }
